cbcbe528f2fece5fff9ac9f05aadf9f003d94603
Sami Malik <samimalik@samis-mbp-4.lan>
2019-10-07 14:13:04 -0400

    Pushing up template sub-theme with libraries override sample

---
+++ themes/custom/test_a/js/test.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * Drupal WxT Bootstrap object.
+ */
+
+/**
+ * All Drupal WxT Bootstrap JavaScript APIs are contained in this namespace.
+ *
+ * @namespace
+ */
+(function($, Drupal) {
+    'use strict';
+
+    Drupal.wxt_bootstrap = {
+        settings: drupalSettings.wxt_bootstrap || {},
+    };
+
+    alert('coming from test.js');
+    /**
+     * Returns the version of WxT being used.
+     *
+     * @return {string}
+     *   The version of WxT being used.
+     */
+    Drupal.wxt_bootstrap.version = 'WxT v4.0.29';
+
+})(window.jQuery, window.Drupal, window.drupalSettings);
