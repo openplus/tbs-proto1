543c4d78c4d8cf7c27e56d7882bcf0c153551082
Sami Malik <samimalik@samis-mbp-4.lan>
2019-10-25 16:44:10 -0400

    Adding a new sub-theme custom_header_footer with templates for the header region

---
+++ themes/custom/custom_header_footer/js/test.js
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * Drupal WxT Bootstrap object.
+ */
+
+/**
+ * All Drupal WxT Bootstrap JavaScript APIs are contained in this namespace.
+ *
+ * @namespace
+ */
+(function($, Drupal) {
+    'use strict';
+
+    Drupal.wxt_bootstrap = {
+        settings: drupalSettings.wxt_bootstrap || {},
+    };
+
+    alert('A custom JS file - loads this alert!');
+    console.log('test');
+    console.log('!!!>>???');
+  
+    /**
+     * Returns the version of WxT being used.
+     *
+     * @return {string}
+     *   The version of WxT being used.
+     */
+    Drupal.wxt_bootstrap.version = 'WxT v4.0.29';
+
+})(window.jQuery, window.Drupal, window.drupalSettings);
