6e85ce830df4a8b5994ebc0a020b778b26f9b41d
Ryan Hyma <ryan.hyma@gmail.com>
2020-02-28 14:07:31 -0500

    Layered menu code moved to theme, so events fire properly.

--- themes/custom/canada_experiments/js/test.js
+++ themes/custom/canada_experiments/js/test.js
@@ -30,5 +30,17 @@
 })(window.jQuery, window.Drupal, window.drupalSettings);
 
 jQuery(document).on("wb-ready.wb", function (event) {
-    console.log("fired");
+    if (window.location.href.indexOf("layered-navigation-master-file") > -1) {
+        let searchParams = new URLSearchParams(window.location.search);
+        if (searchParams.has('t')) {
+            let a = $('a').filter(function(index) { return $(this).text() === searchParams.get("t"); });
+            a.trigger('mouseenter');
+        }
+        $("#wb-auto-3 *").on("mouseleave",function() {
+            event.stopPropagation();
+        });
+        $("#wb-auto-3 *").on("mouseenter",function() {
+            event.stopPropagation();
+        });
+    }
 });
